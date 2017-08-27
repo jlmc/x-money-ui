35269397fe4174be1818b46b7304fe7cb6d25fd0
costajlmpp <costajlmpp@gmail.com>
2017-08-28 00:57:48 +0100

    13.2 add primeNg calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {InputTextareaModule, TabViewModule, TooltipModule} from 'primeng/primeng';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {
+  CalendarModule,
+  DataTableModule,
+  InputTextareaModule,
+  SharedModule,
+  TabViewModule,
+  TooltipModule
+} from 'primeng/primeng';
 // acording the PrimeNG documentation it could be better import the compoments one by one
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
-import { ButtonModule } from 'primeng/components/button/button';
-import {DataTableModule, SharedModule} from 'primeng/primeng';
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import {ButtonModule} from 'primeng/components/button/button';
 
-import { AppComponent } from './app.component';
-import { AppActivitiesSearchComponent } from './app-activities-search/app-activities-search.component';
-import { AppNavBarComponent } from './app-nav-bar/app-nav-bar.component';
-import { AppPersonsSearchComponent } from './app-persons-search/app-persons-search.component';
-import { AppActivityRegisterComponent } from './app-activity-register/app-activity-register.component';
+import {AppComponent} from './app.component';
+import {AppActivitiesSearchComponent} from './app-activities-search/app-activities-search.component';
+import {AppNavBarComponent} from './app-nav-bar/app-nav-bar.component';
+import {AppPersonsSearchComponent} from './app-persons-search/app-persons-search.component';
+import {AppActivityRegisterComponent} from './app-activity-register/app-activity-register.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,6 +36,8 @@ import { AppActivityRegisterComponent } from './app-activity-register/app-activi
     , SharedModule
     , TooltipModule
     , InputTextareaModule
+    , CalendarModule
+    , BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
