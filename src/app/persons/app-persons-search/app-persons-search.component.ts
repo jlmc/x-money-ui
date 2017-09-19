95fc2bfee57c1223fdf50db03952bd71e23a732c
costajlmpp <costajlmpp@gmail.com>
2017-09-19 22:52:18 +0100

    17.7-search persons

--- src/app/persons/app-persons-search/app-persons-search.component.ts
+++ src/app/persons/app-persons-search/app-persons-search.component.ts
@@ -1,63 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PersonsService} from '../persons.service';
 
 @Component({
   selector: 'app-persons-search',
   templateUrl: './app-persons-search.component.html',
   styleUrls: ['./app-persons-search.component.css']
 })
-export class AppPersonsSearchComponent {
+export class AppPersonsSearchComponent implements OnInit {
 
-  persons = [
-    {
-      code: '1',
-      name: 'Cristiano Ronaldo',
-      active: true,
-      address: {
-        city: 'Funchal',
-        street: 'Eng. tico',
-        zipCode: '234-432'
-      }
-    },
-    {
-      code: '2',
-      name: 'Lionel Messi',
-      active: true,
-      address: {
-        city: 'Barcelona',
-        street: 'Camp now',
-        zipCode: '1234-789'
-      }
-    },
-    {
-      code: '3',
-      name: 'Zatan ibrahimovic',
-      active: false,
-      address: {
-        city: 'manchester',
-        street: 'old trafford',
-        zipCode: '9878-789'
-      }
-    },
-    {
-      code: '4',
-      name: 'Luisão',
-      active: true,
-      address: {
-        city: 'Lisbon',
-        street: 'estadio da Luz',
-        zipCode: '1111-111'
-      }
-    },
-    {
-      code: '5',
-      name: 'Diego Maradona',
-      active: false,
-      address: {
-        city: 'Bueno Aires',
-        street: 'la bombonera',
-        zipCode: '1111-111'
-      }
-    }
-  ];
+  persons: any[] = [];
 
+  constructor(private personsServices: PersonsService) {}
+
+  ngOnInit(): void {
+    this.personsServices.search().then(ps => {
+      this.persons = ps;
+    });
+  }
 }
